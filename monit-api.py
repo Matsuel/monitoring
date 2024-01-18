666f41be0f900b4f3292206f59a8dd3eb78b3113
Matsuel <matsuel0308@gmail.com>
2024-01-18 10:52:09 +0100

    api create report

--- monit-api.py
+++ monit-api.py
@@ -1,18 +1,21 @@
 from  flask import Flask, jsonify, request
 import json
-from monit import get_report, get_all_reports, get_last_report, get_reports_younger_than, get_avg_of_report, create_report_directory, create_config_directory, log
+from monit import get_report, get_all_reports, get_last_report, get_avg_of_report, create_report
 
 app = Flask(__name__)
 directory  = "./monit"
 
+#Get all reports names
 @app.route('/api/v1.0/list', methods=['GET'])
 def get_reports():
     return jsonify({"reports": get_all_reports(directory)})
 
+#Get last report content
 @app.route('/api/v1.0/last', methods=['GET'])
 def get_last():
     return jsonify(get_last_report(directory))
 
+#Get report content by name if name is provided
 @app.route('/api/v1.0/report', methods=['GET'])
 def name_required():
     return jsonify({"error": "Name required"})
@@ -24,6 +27,8 @@ def get_report_by_name(name):
     else:
         return jsonify(get_report(f"{name}.json", directory))
     
+
+#Get reports younger than hours if hours is provided
 @app.route('/api/v1.0/avg', methods=['GET'])
 def hours_required():
     return jsonify({"error": "Hours required"})
@@ -31,6 +36,11 @@ def hours_required():
 @app.route('/api/v1.0/avg/<int:hours>', methods=['GET'])
 def get_avg(hours):
     return jsonify(get_avg_of_report(hours, directory))
+
+@app.route('/api/v1.0/check', methods=['GET'])
+def check():
+    create_report()
+    return jsonify({"report": get_last_report(directory)})
 
 
 if (__name__ == "__main__"):
