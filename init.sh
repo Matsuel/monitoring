56a45691d516c4df5c1c9cdf69ec015c84c78e97
Matsuel <matsuel0308@gmail.com>
2024-01-26 09:22:48 +0100

    add user

--- init.sh
+++ init.sh
@@ -22,15 +22,17 @@ mkdir -p /var/log/monit /etc/monit/conf.d /var/monit
 # Création d'un utilisateur monit qui sera utilisé pour lancer le script monit.py
 useradd -m monit
 
+# Copie le fichier config.json dans le dossier /etc/monit/conf.d
+cp config.json /etc/monit/conf.d/
+cp monit.service monit.timer /etc/systemd/system/
+cp monit.py /var/monit/
+
 # Change la propriété des dossiers à l'utilisateur actuellement connecté afin qu'il puisse écrire les rapports dedans
 chown -R monit:monit /var/log/monit
 chown -R monit:monit /etc/monit/conf.d
 chown -R monit:monit /var/monit
 
-# Copie le fichier config.json dans le dossier /etc/monit/conf.d
-cp config.json /etc/monit/conf.d/
-cp monit.service monit.timer /etc/systemd/system/
-cp monit.py /var/monit/
+
 
 # Démarrage automatique de monit dès le démarrage de la machine et démarrage du timer maintenant afin qu'il commence à créer des rapports
 systemctl daemon-reload
