488ca432499b753fde944536b4b2a1c9b6a20931
Matsuel <matsuel0308@gmail.com>
2024-02-01 09:29:37 +0100

    script

--- init.sh
+++ init.sh
@@ -8,8 +8,15 @@ fi
 # Install python3 and pip3 if not installed
 if ! [ -x "$(command -v python3)" ]; then
     echo 'Error: python is not installed.' >&2
-    dnf python3 python3-pip
+    dnf python3-pip
     echo "Python3 installed"
+    exit 1
+fi
+
+if ! [ -x "$(command -v pip3)" ]; then
+    echo 'Error: pip is not installed.' >&2
+    dnf python3-pip
+    echo "Pip3 installed"
     exit 1
 fi
 
